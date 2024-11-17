b79f8695904f945c10c092f819b46c2051b02e1c
hhman24 <anhoang483@gmail.com>
2024-11-17 16:04:58 +0700

    feat: init intelliji

---
+++ src/main/java/com/hhman/book_network/BookNetworkServerApplication.java
@@ -0,0 +1,13 @@
+package com.hhman.book_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhman/book_network/BookNetworkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhman.book_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
